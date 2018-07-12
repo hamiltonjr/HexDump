72c8f450fce8fc50c4be58ae4e57d7518a1c0e1b
Hamilton Gonçalves Jr <hamiltonjr2010@gmail.com>
2018-07-12 14:45:01 -0300

    Ajustes e a inclusão de arquivos de teste

--- hexdump.c
+++ hexdump.c
@@ -3,52 +3,88 @@
 #include <string.h>
 #include <limits.h>
 
+long size(char*);
+void stradj(char*);
+void spaces(int);
+
 int main(int argc, char** argv) {
     char *buffer = (char*)malloc(17);
-    char *file = (char*)malloc(255);
-    FILE *fpin;     
-    int i, count;
+    int line, column, little;
+    long bytes, lines;
+    FILE *fpin;
+    char c = 0;
 
-    if (argc==2) {
-        strcpy(file, argv[1]);
-    } else {
+    // checa argumento (nome do arquivo)
+    if (argc != 2) {
         printf("Erro!\n");
         exit(EXIT_FAILURE);
     }
 
-    if ((fpin = fopen(file, "r")) == NULL) {
+    bytes = size(argv[1]);
+    lines = bytes / 16L;
+    little = bytes % 16;
+
+    // checa validade do ponteiro para arquivo
+    if ((fpin = fopen(argv[1], "r")) == NULL) {
         printf("Erro\n");
         exit(EXIT_FAILURE);
     }
 
-    // lê 16 bytes do arquivo
-    count = 0;
-    while (fread (buffer, 16, 1, fpin) == 1) {
-        // mostra offset
-        printf("%08X  ", count);
-        count += 16;
+    line = 0;
+    column = 0;
+    while ((c = getc(fpin)) != EOF) {
+        if (column == 0)
+            printf("%08X  ", 16*line);
 
-        // mostra primeiro chunck
-        for (i=0; i<8 && buffer[i]!='\0'; i++) {
-            printf("%02X ", buffer[i]);
-        }
-        printf(" ");
+        printf("%02X ", c);
+        buffer[column] = c;
 
-        // mostra segundo chunck
-        for (i=i+1; i<16 && buffer[i]!='\0'; i++) {
-            printf("%02X ", buffer[i]);
-        }
+        if (column == 15) {
+            column = 0;
+            line++;
+            stradj(buffer);
+            printf("|%s|\n", buffer);
+        } else
+            column++;
+    }
 
-        // mostra string
-        for (int i=0; i<strlen(buffer); i++) {
-            if (buffer[i]<32 || buffer[i]>127) {
-                buffer[i] = '.';
-            }
-        }
-        printf(" |%s|\n", buffer);
-    }    
+    // na última linha
+    buffer[column] = '\0';
+    spaces(48 - 3*little);
+    stradj(buffer);
+    printf("|%s|\n", buffer);
+
+    //printf("Spaces: %d\n", 50-3*little);
+    //printf("Bytes: %ld\n", bytes);
+    //printf("Lines: %ld\n", lines);
+    //printf("Little: %d\n", little);
 
     fclose(fpin);
     return 0;
 }
 
+long size(char *file) {
+    FILE *fp;
+    long bytes;
+
+    if ((fp = fopen(file, "r")) == NULL) {
+        printf("Erro\n");
+        exit(EXIT_FAILURE);
+    }
+    fseek(fp, 0, SEEK_END);
+    bytes = ftell(fp);
+    fclose(fp);
+    return bytes;
+}
+
+
+void stradj(char* s) {
+    for (int i=0; s[i]!='\0'; i++)
+        if (s[i] < 32)
+            s[i] = '.';
+}
+
+void spaces(int x) {
+    for (int i=0; i<x; i++)
+        printf(" ");
+}
